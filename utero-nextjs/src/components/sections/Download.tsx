cbe7b1d92e6cc8de939b713ab381014336f4422d
Irilbos <rilamiril701@gmail.com>
2026-09-02 13:29:50 +0700

    fix: remove unused download item profiles for clarity and maintenance

--- utero-nextjs/src/components/sections/Download.tsx
+++ utero-nextjs/src/components/sections/Download.tsx
@@ -55,39 +55,11 @@ const downloadItems = [
     alt: "Download company profile Utero Legal",
   },
   {
-    label: "Profile Utero Academy",
-    desc: "Company profile divisi academy Utero",
-    href: "",
-    img: "/images/download/utero-academy.svg",
-    alt: "Download company profile Utero Academy",
-  },
-  {
-    label: "Profile Carubra",
-    desc: "Company profile Carubra",
-    href: "",
-    img: "/images/download/carubra.svg",
-    alt: "Download company profile Carubra",
-  },
-  {
-    label: "Profile Buzzerhood",
-    desc: "Company profile Buzzerhood",
-    href: "",
-    img: "/images/download/buzzerhood.svg",
-    alt: "Download company profile Buzzerhood",
-  },
-  {
     label: "Profile Soundpub",
     desc: "Company profile Soundpub",
     href: "/images/download/compro Soundpub_Music_Ecosystem.pdf",
     img: "/images/download/soundpub.svg",
     alt: "Download company profile Soundpub",
-  },
-  {
-    label: "Profile Smartsuco",
-    desc: "Company profile Smartsuco",
-    href: "",
-    img: "/images/download/smartsuco.svg",
-    alt: "Download company profile Smartsuco",
   },
 ];
 
